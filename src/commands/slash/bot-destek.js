9e695e1992789bd8efcb54fec89f18f22289dcba
shiva <159827394+shivaxrq@users.noreply.github.com>
2024-04-04 13:20:13 +0300

    Update bot-destek.js

--- src/commands/slash/bot-destek.js
+++ src/commands/slash/bot-destek.js
@@ -1,12 +1,6 @@
 const { EmbedBuilder, PermissionsBitField } = require("discord.js");
-const { SlashCommandBuilder } = require("@discordjs/builders");
-
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("destek")
-    .setDescription("Destek sunucusunu gösterir."),
-    // komutu geliştirmek istersen guide: https://discordjs.guide/slash-commands/advanced-creation.html
+discordjs.guide/slash-commands/advanced-creation.html
     run: async (client, interaction) => {
-      interaction.reply(`discord.gg/egemen`)
+      interaction.reply(``)
     }
  };
